94adba5137bc892e71eeaecc3992b622ab255637
Lucas Eduardo <146371860+Lucaseduardo583@users.noreply.github.com>
2025-02-24 16:43:00 -0300

    atualizacao

--- Atividade_1/Form1.Designer.cs
+++ Atividade_1/Form1.Designer.cs
@@ -28,34 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btn_converter = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_cotacao = new System.Windows.Forms.TextBox();
+            this.txt_valor = new System.Windows.Forms.TextBox();
+            this.lbl_resultado = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // btn_converter
             // 
-            this.button1.Location = new System.Drawing.Point(153, 158);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btn_converter.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btn_converter.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_converter.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_converter.Location = new System.Drawing.Point(73, 207);
+            this.btn_converter.Name = "btn_converter";
+            this.btn_converter.Size = new System.Drawing.Size(105, 33);
+            this.btn_converter.TabIndex = 0;
+            this.btn_converter.Text = "Converter";
+            this.btn_converter.UseVisualStyleBackColor = false;
+            this.btn_converter.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(49, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Cotação do Dólar";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(70, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Valor em Dólares";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(39, 265);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(179, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Resultado da Conversão";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // txt_cotacao
+            // 
+            this.txt_cotacao.Location = new System.Drawing.Point(53, 68);
+            this.txt_cotacao.Name = "txt_cotacao";
+            this.txt_cotacao.Size = new System.Drawing.Size(145, 20);
+            this.txt_cotacao.TabIndex = 4;
+            // 
+            // txt_valor
+            // 
+            this.txt_valor.Location = new System.Drawing.Point(53, 160);
+            this.txt_valor.Name = "txt_valor";
+            this.txt_valor.Size = new System.Drawing.Size(145, 20);
+            this.txt_valor.TabIndex = 5;
+            // 
+            // lbl_resultado
+            // 
+            this.lbl_resultado.AutoSize = true;
+            this.lbl_resultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_resultado.Location = new System.Drawing.Point(12, 315);
+            this.lbl_resultado.Name = "lbl_resultado";
+            this.lbl_resultado.Size = new System.Drawing.Size(0, 13);
+            this.lbl_resultado.TabIndex = 6;
+            this.lbl_resultado.Click += new System.EventHandler(this.lbl_resultado_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(668, 381);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(257, 352);
+            this.Controls.Add(this.lbl_resultado);
+            this.Controls.Add(this.txt_valor);
+            this.Controls.Add(this.txt_cotacao);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_converter);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_converter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_cotacao;
+        private System.Windows.Forms.TextBox txt_valor;
+        private System.Windows.Forms.Label lbl_resultado;
     }
 }
 
--- Atividade_1/Form1.cs
+++ Atividade_1/Form1.cs
@@ -22,6 +22,30 @@ namespace Atividade_1
         {
             // 1- Faça um programa que: -Leia a cotação do dólar - Leia um valor em dólares - Converta esse valor para Real -Mostre o resultado
 
+            try
+            {
+                double cotacaoDolar = Convert.ToDouble(txt_cotacao.Text);
+                double valorDolar = Convert.ToDouble(txt_valor.Text);
+
+                double valorReal = valorDolar * cotacaoDolar;
+
+                lbl_resultado.Text = $"O valor de {valorDolar} dólares equivale a {valorReal} reais.";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Por favor, insira valores válidos para a cotação e o valor em dólares.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbl_resultado_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
