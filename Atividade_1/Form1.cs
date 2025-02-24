3a51b02e8c5d1f9038dd4fa1fedcc4340868a83f
Lucas Eduardo <146371860+Lucaseduardo583@users.noreply.github.com>
2025-02-24 16:07:49 -0300

    Adicionar arquivos de projeto.

---
+++ Atividade_1/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Resources.ResXFileRef;
+
+namespace Atividade_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 1- Faça um programa que: -Leia a cotação do dólar - Leia um valor em dólares - Converta esse valor para Real -Mostre o resultado
+
+        }
+    }
+}
